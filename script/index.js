291ca05375c2cce5d0a74c5ddd3359d671f45b7d
miljanjov <miljan_jovovic@yahoomail.com>
2022-04-30 01:00:24 +0200

    Clock writtecode written only with JS and CSS

---
+++ script/index.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+
+  const minuts = now.getMinutes();
+  const minDegrees = (minuts / 60) * 360 + 90;
+
+  const houers = now.getHours();
+  const hourDegrees = (houers / 60) * 360 + 90;
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minHand.style.transform = `rotate(${minDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees})deg`;
+}
+
+setInterval(setDate, 1000);
